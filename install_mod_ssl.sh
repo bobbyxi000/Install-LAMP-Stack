1c6714545be471df91b7e7a7de4a28115b496cc9
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-08-27 01:20:44 +0800

    Update install_mod_ssl.sh

--- install_mod_ssl.sh
+++ install_mod_ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/usr/bin/env bash
 # install mod_ssl and self-signed certificate
-yum install mod_ssl
+yum install mod_ssl -y
 mkdir /etc/ssl/private
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
 
