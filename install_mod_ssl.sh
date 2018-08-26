a89f3d7a17e4f219561ee4468cc82952ad00d3bd
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-08-27 01:16:57 +0800

    Install mod_ssl and self-signed cert

---
+++ install_mod_ssl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/usr/bin/env bash
+# install mod_ssl and self-signed certificate
+yum install mod_ssl
+mkdir /etc/ssl/private
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
+
+
+
+
