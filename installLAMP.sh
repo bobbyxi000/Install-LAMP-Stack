5891fb6de0954ab6bdd978c923939720ae758c7e
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-08-26 23:51:10 +0800

    Modification. Start and enable service

--- installLAMP.sh
+++ installLAMP.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 yum install mariadb mariadb-server httpd -y
-systemctl enable mariadb
-systemctl enable httpd
+systemctl start httpd mariadb
+systemctl enable mariadb httpd
