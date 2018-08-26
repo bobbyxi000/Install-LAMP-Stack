62e3f9554b72eaaa48274a35d1b753775711f838
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-08-27 01:19:43 +0800

    Install LAMP

--- installLAMP.sh
+++ installLAMP.sh
@@ -5,3 +5,4 @@ curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 yum install mariadb mariadb-server httpd -y
 systemctl start httpd mariadb
 systemctl enable mariadb httpd
+
