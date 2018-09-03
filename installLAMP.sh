86654c72920c72430c23b86cb944b8fcf0732f14
Bobby F <40128726+bobbyxi000@users.noreply.github.com>
2018-09-04 01:07:42 +0800

    Update installLAMP.sh

--- installLAMP.sh
+++ installLAMP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/usr/bin/env bash
 # install apache and latest version of mariadb
-# wget -O - https://raw.githubusercontent.com/bobbyxi000/Install-LAMP-Stack/master/installLAMP.sh
+# wget -O - https://raw.githubusercontent.com/bobbyxi000/Install-LAMP-Stack/master/installLAMP.sh | sudo bash
 curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 yum install mariadb mariadb-server httpd -y
 systemctl start httpd mariadb
